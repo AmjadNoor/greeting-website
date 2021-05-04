ba4c220ce69467266cbb2bd4ddc8c3e0956fedbf
Amjadnoor <nooramjadmail.com@gmail.com>
2021-05-04 16:09:14 +0530

    Update

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Heading from './Heading'
+
+class App extends Component {
+  return
+  <Heading />
+}
+
+export default App;
---
+++ src/Heading.jsx
@@ -0,0 +1,10 @@
+import React, {
+  Component
+} from 'react';
+
+class Heading extends Component {
+  return
+  <h1>Hello World!</h1>
+};
+
+export default Heading;
